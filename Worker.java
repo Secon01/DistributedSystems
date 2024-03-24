bc769f8759098b9324d322844e3d2dcb67273bdf
Secon01 <73309141+Secon01@users.noreply.github.com>
2024-03-24 20:44:12 +0200

    multithreading _v1

--- Worker.java
+++ Worker.java
@@ -1,16 +1,12 @@
-import java.rmi.Naming;
-import java.rmi.registry.LocateRegistry;
-
-public class Worker
+public class Worker extends Thread
 {
-    public static void main(String[] args) {
-        try 
-        {
-            PrintName obj = new PrintName();
-            LocateRegistry.createRegistry(1900);
-            Naming.rebind("rmi://localhost:1900" + "/secon", obj);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+    public void run()
+    {
+        System.out.println("Hello I am a worker");
+    }
+
+    Worker(String name)
+    {
+        this.setName(name);
     }
 }
