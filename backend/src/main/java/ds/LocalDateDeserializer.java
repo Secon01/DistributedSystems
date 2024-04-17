e0bfb79b690f8872b7d29b5ceba5509988443881
papanikolaoustef <75268324+papanikolaoustef@users.noreply.github.com>
2024-04-17 15:00:57 +0300

    Update LocalDateDeserializer.java

--- backend/src/main/java/ds/LocalDateDeserializer.java
+++ backend/src/main/java/ds/LocalDateDeserializer.java
@@ -4,10 +4,10 @@ import com.google.gson.*;
 import java.lang.reflect.Type;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-
+// Sets the appropriate format of the date input
 public class LocalDateDeserializer implements JsonDeserializer<LocalDate> {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-
+// Takes the date-element and returns it in the appropriate format
     @Override
     public LocalDate deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
         String dateString = json.getAsString();
