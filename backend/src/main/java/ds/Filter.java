7ba7113b36e3bfe2e8b11d802aef41f0d3af7496
papanikolaoustef <75268324+papanikolaoustef@users.noreply.github.com>
2024-04-17 12:51:14 +0300

    Update Filter.java

--- backend/src/main/java/ds/Filter.java
+++ backend/src/main/java/ds/Filter.java
@@ -1,16 +1,15 @@
 package ds;
-
 import java.lang.reflect.Field;
 
 // Filters holder
 public class Filter extends Request
 {
-    private String area;
-    private DateRange dateRange;
-    private int guests;
-    private double price;
-    private int stars;
-
+    private String area;                          // area searching
+    private DateRange dateRange;                  // daterange searching
+    private int guests;                           // number of guests searching
+    private double price;                         // price searching
+    private int stars;                            // stars searching
+    //Constructtor
     Filter()
     {
         this.area = null;
@@ -19,55 +18,44 @@ public class Filter extends Request
         this.price = 0.0;
         this.stars = 0;
     }
-
     // Setters for filter attributes
     public void setArea(String a)
     {
         this.area = a;
     }
-
     public void setDate(String start, String end)
     {
         this.dateRange.setStartDate(start);      // set end date of date range 
         this.dateRange.setEndDate(end);          // set end date of date range 
     }
-
     public void setGuests(int g)
     {
         this.guests = g;
     }
-
     public void setPrice(double p)
     {
         this.price = p;
     }
-
     public void setStars(int s)
     {
         this.stars = s;
     }
-
     // Getters
     public String getArea() {
         return area;
     }
-
     public DateRange getDate() {
         return dateRange;
     }
-
     public int getGuests() {
         return guests;
     }
-
     public double getPrice() {
         return price;
     }
-
     public int getStars() {
         return stars;
     }
-
     // Computes how many non null or 0 values does this object has
     public int numNonZero()
     {
